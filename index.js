767cb27510cef33cb9809357a27f234cded50b69
massi <massi@laptop21.(none)>
2014-10-24 19:04:00 +0200

    added context for query strategy

--- index.js
+++ index.js
@@ -2,5 +2,7 @@
  * @author Massimiliano Izzo
  */
 var PostgresJSONQueryStrategy = require('lib/PostgresJSONQueryStrategy');
+var QueryBuilder = require('lib/QueryBuilder');
 
+module.exports.QueryBuilder = QueryBuilder;
 module.exports.PostgresJSONQueryStrategy = PostgresJSONQueryStrategy;
---
+++ test/QueryBuilder-test.js
@@ -0,0 +1,33 @@
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var QueryBuilder = require('../lib/QueryBuilder');
+var PostgresJSONQueryStrategy = require('../lib/PostgresJSONQueryStrategy.js');
+
+describe('#QueryBuilder', function() {
+    
+    describe('#constructor', function() {
+        
+        it('should have a strategy property with a compose method', function() {
+            var builder = new QueryBuilder();
+            expect(builder).to.have.property('strategy');
+            expect(builder.strategy).to.be.an.instanceof(PostgresJSONQueryStrategy);
+        });
+
+    });
+
+    describe('#compose', function() {
+        
+        beforeEach(function() {
+            this.builder = new QueryBuilder();
+            sinon.spy(this.builder.strategy, "compose");
+        });
+
+        it('should call the compose method of the strategy object', function() {
+            var params = { pivotDataType: {id: 1, name:'Test'}};
+            this.builder.compose(params);
+            expect(this.builder.strategy.compose.calledOnce);
+        });
+
+    });
+
+});
